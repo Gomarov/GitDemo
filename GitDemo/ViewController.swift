86265875d6f3f48a1f7c6ec03d2d108880f0571d
Pavel Gomarov <p.gomarov@icloud.com>
2020-05-01 22:17:11 +0300

    Inicial commit

---
+++ GitDemo/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GitDemo
+//
+//  Created by Павел on 01.05.2020.
+//  Copyright © 2020 Павел. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
