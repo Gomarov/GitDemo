36dca3fba80bc93cfdd7a63eb52fe27c97fe565a
Gomarov <63294995+Gomarov@users.noreply.github.com>
2020-05-02 21:21:41 +0300

    Update AppDeligate.swift

--- GitDemo/AppDelegate.swift
+++ GitDemo/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         
         print ("Hello world")
+        print ("1232314340349")
         
         return true
     }
